e2f94bfc9cd4c93590abe56ea0ba97e5cf44c0af
sourabhsarkar <sourabhsarkar58@hotmail.com>
2016-03-27 01:02:06 +0530

    Initial commit

---
+++ app/src/main/java/com/chatapp/chitchat/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.chatapp.chitchat;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    protected TextView mSignUpTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mSignUpTextView = (TextView)findViewById(R.id.signUpText);
+        mSignUpTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
